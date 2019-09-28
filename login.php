dc5a64862c60e7bc082a7e98a9f4b41b43ea49c4
Vykio <alexandrevasseur06@gmail.com>
2019-09-28 18:35:02 +0200

    Page login créée
    Ajout de la page login (Design uniquement) et de deux fichiers CSS (Skeleton CSS) pour le design de la page

---
+++ login.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Authentification</title>
+	<link rel="stylesheet" type="text/css" href="src/css/skeleton/normalize.css">
+	<link rel="stylesheet" type="text/css" href="src/css/skeleton/skeleton.css">
+
+	<link rel="stylesheet" type="text/css" href="src/css/login/login.css">
+</head>
+
+<body>
+
+<div id="header" class="header" style="/*ackground-image: url('imgs/abstract_login.png');*/ background-color: lightblue">
+		
+	</div>
+
+	
+<div class="container login" style="border: 0px solid black">
+
+	
+
+	<div id="login" class="super">
+
+	  	<form>
+	  		
+
+	  		<div class="lower">
+	  			<h2><strong>Connexion au service en ligne</strong></h2>
+	  			<div class="row">
+		      <label for="exampleEmailInput">Nom d'utilisateur</label>
+		      <input class="u-full-width" type="text" placeholder="Utilisateur" id="userInput" style="border-radius: 50px;" required>
+		    
+		  </div>
+		  <div class="row">
+		      <label for="exampleEmailInput">Mot de passe</label>
+		      <input class="u-full-width" type="password" placeholder="Mot de passe" id="passwordInput" style="border-radius: 50px;" required>
+		    
+		  </div>
+		  <br>
+		  <input class="u-full-width button-primary" type="submit" value="Connexion" style="border-radius: 50px;">
+	  		</div>
+		  
+		</form>
+
+
+	</div>
+
+</div>
+
+
+	
+
+</body>
+</html>
