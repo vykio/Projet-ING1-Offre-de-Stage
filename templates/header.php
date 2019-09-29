7ae6f393af62f24d9e191e4e2ce45f1784bbdbbf
Vykio <alexandrevasseur06@gmail.com>
2019-09-29 22:05:01 +0200

    Utilisation des templates
    En php, pour réduire le code à écrire pour chaque page

---
+++ templates/header.php
@@ -0,0 +1,5 @@
+<title><?php echo PAGE_NAME ?></title>
+
+<!-- Skeleton CSS -->
+<link rel="stylesheet" type="text/css" href="src/css/skeleton/normalize.css">
+<link rel="stylesheet" type="text/css" href="src/css/skeleton/skeleton.css">
