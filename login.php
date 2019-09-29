f2d2f9de6f3ebc53a22fcb7ac098d74fee260ae2
Vykio <alexandrevasseur06@gmail.com>
2019-09-29 15:02:13 +0200

    Login.php commenté
    Ajout d'un Footer et de plusieurs commentaires pour comprendre le code

--- login.php
+++ login.php
@@ -3,46 +3,59 @@
 <html>
 <head>
 	<title>Authentification</title>
+
+	<!-- Skeleton CSS -->
 	<link rel="stylesheet" type="text/css" href="src/css/skeleton/normalize.css">
 	<link rel="stylesheet" type="text/css" href="src/css/skeleton/skeleton.css">
 
+	<!-- CSS custom -->
 	<link rel="stylesheet" type="text/css" href="src/css/login/login.css">
 </head>
 
 <body>
 
-<div id="header" class="header" style="background: url('imgs/login.jpg') no-repeat center center fixed; background-color: #EEEEEE;
+<!-- Image de fond pour tous les navigateurs -->
+<div class="header" style="background: url('imgs/login.jpg') no-repeat center center fixed; background-color: #EEEEEE;
 	-webkit-background-size: cover;
 	-moz-background-size: cover;
 	-o-background-size: cover;
 	background-size: cover;">
-		
-	</div>
+</div>
 
 	
+<!-- Container centré et réduit par Skeleton.css et re-réduit par la classe CSS "login" -->
 <div class="container login" style="border: 0px solid black">
 
 	
-
+	<!-- div blanche sur le site contenant la form -->
 	<div id="login" class="super">
 
 	  	<form>
 	  		
-
+	  		<!-- Contenu dans la form pour mettre un padding left et right sur le contenu de la forme -->
 	  		<div class="lower">
+
 	  			<h2><strong>Connexion au service en ligne</strong></h2>
+
+	  			<!-- Message à afficher quand erreur connexion -->
+	  			<div id="message_erreur_login" class="erreur_login" style="display: none">Erreur, le nom d'utilisateur ou le mot de passe est incorrect</div>
+
+	  			<!-- class = "row" créé par Skeleton CSS pour faire une "ligne" -->
 	  			<div class="row">
-		      <label for="usernameInput">Nom d'utilisateur</label>
-		      <input class="u-full-width" type="text" placeholder="Utilisateur" id="usernameInput" style="border-radius: 50px;" required>
-		    
-		  </div>
-		  <div class="row">
-		      <label for="passwordInput">Mot de passe</label>
-		      <input class="u-full-width" type="password" placeholder="Mot de passe" id="passwordInput" style="border-radius: 50px;" required>
-		    
-		  </div>
-		  <br>
-		  <input class="u-full-width button-primary" type="submit" value="Connexion" style="border-radius: 50px;">
+					<label for="usernameInput">Nom d'utilisateur</label>
+					<!-- Classe u-full-width créée par Skeleton.css et permet de mettre la longueur (width) au maximum de la div dans lequel il est contenu -->
+					<input class="u-full-width" type="text" placeholder="Utilisateur" id="usernameInput" style="border-radius: 50px;" required>
+				</div>
+
+		  		<div class="row">
+					<label for="passwordInput">Mot de passe</label>
+		      		<input class="u-full-width" type="password" placeholder="Mot de passe" id="passwordInput" style="border-radius: 50px;" required>
+		  		</div>
+		  		<br>
+		  		<!-- button-primary créé par Skeleton.css et change la couleur du bouton par la couleur primaire (à changer par la couleur de l'école) -->
+		  		<!-- type="submit" pour confirmer la form -->
+		  		<input class="u-full-width button-primary" type="submit" value="Connexion" style="border-radius: 50px;">
+	  		
 	  		</div>
 		  
 		</form>
@@ -50,6 +63,11 @@
 
 	</div>
 
+</div>
+
+
+<div class="footer">
+	Projet informatique - 1ère année Ecole d'Ingénieurs du Littoral Côte d'Opale - Groupe TP12
 </div>
 
 
