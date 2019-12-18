e738b1c02b8726ef50cb1931657b6d7c6f4a03c8
Vykio <alexandrevasseur06@gmail.com>
2019-12-18 10:13:14 +0100

    Update annonce.php

--- annonce.php
+++ annonce.php
@@ -27,100 +27,127 @@ if (isset($_GET['id']) && !empty($_GET['id'])) {
 		// echo "<span style=\"white-space: pre-line;\">" . $annonce["description"] ."</span>";
 
 		if (isset($_POST['confirm']) && $_POST['confirm'] == 'Envoyer') {
-			if (isset($_FILES['uploadedFile']) && $_FILES['uploadedFile']['error'] === UPLOAD_ERR_OK) {
-				// get details of the uploaded file
-				$fileTmpPath = $_FILES['uploadedFile']['tmp_name'];
-				$fileName = $_FILES['uploadedFile']['name'];
-				$fileSize = $_FILES['uploadedFile']['size'];
-				$fileType = $_FILES['uploadedFile']['type'];
-				$fileNameCmps = explode(".", $fileName);
-				$fileExtension = strtolower(end($fileNameCmps));
 
-				//Sanitize !
-				$newFileName = md5(time() . $fileName) . '.' . $fileExtension;
+			if (isset($_POST["motivation"]) && !empty($_POST["motivation"]) && isset($_POST["email_of_user"]) && !empty($_POST["email_of_user"])) {
+				$motivation = $_POST["motivation"];
+				$email_of_user = $_POST["email_of_user"];
 
-				$allowedfileExtensions = array('pdf', 'doc', 'docx', 'odt', 'rtf');
+				if (isset($_FILES['uploadedFile']) && $_FILES['uploadedFile']['error'] === UPLOAD_ERR_OK) {
+					// get details of the uploaded file
+					$fileTmpPath = $_FILES['uploadedFile']['tmp_name'];
+					$fileName = $_FILES['uploadedFile']['name'];
+					$fileSize = $_FILES['uploadedFile']['size'];
+					$fileType = $_FILES['uploadedFile']['type'];
+					$fileNameCmps = explode(".", $fileName);
+					$fileExtension = strtolower(end($fileNameCmps));
 
-				if (in_array($fileExtension, $allowedfileExtensions)) {
+					//Sanitize !
+					$newFileName = md5(time() . $fileName) . '.' . $fileExtension;
 
-					// directory in which the uploaded file will be moved
-					$uploadFileDir = 'uploads/';
-					$dest_path = $uploadFileDir . $newFileName;
-					 
-					if(move_uploaded_file($fileTmpPath, $dest_path))
-					{
-					  $message ='File is successfully uploaded.';
+					$allowedfileExtensions = array('pdf', 'doc', 'docx', 'odt', 'rtf');
 
-					  //OK envoie du mail
-						
+					if (in_array($fileExtension, $allowedfileExtensions)) {
 
-						// To
-						$to = database::query("SELECT contact_mail FROM utilisateurs WHERE id=:user_id", array(':user_id'=>$annonce["user_id"]))[0]["contact_mail"];
+						// directory in which the uploaded file will be moved
+						$uploadFileDir = 'uploads/';
+						$dest_path = $uploadFileDir . $newFileName;
 						 
-						// Subject
-						$subject = 'étu-stage - Nouvelle Candidature [' . $annonce["titre"] . ']';
-						 
-						// clé aléatoire de limite
-						$boundary = md5(uniqid(microtime(), TRUE));
-						 
-						// Headers
-						$headers = 'From: étu-stage <admin@vykio.fr>'."\r\n";
-						$headers .= 'Mime-Version: 1.0'."\r\n";
-						$headers .= 'Content-Type: multipart/mixed;boundary='.$boundary."\r\n";
-						$headers .= "\r\n";
-						 
-						// Message pour les clients ne supportant pas le type MIME
-						$msg = 'Texte affiché par des clients mail ne supportant pas le type MIME.'."\r\n\r\n";
-						 
-						// Message HTML
-						$msg .= '--'.$boundary."\r\n";
-						$msg .= 'Content-type: text/html; charset=utf-8'."\r\n\r\n";
-						$msg .= '
-						<center>Code <strong>affiché</strong></center>
-						<div style="">Code test reçu par des utilisateurs</div>
-						'."\r\n";
-						 
-						// Pièce jointe 1
-						$file_name = $dest_path;
-						if (file_exists($file_name))
+						if(move_uploaded_file($fileTmpPath, $dest_path))
 						{
-							$file_type = filetype($file_name);
-							$file_size = filesize($file_name);
-						 
-							$handle = fopen($file_name, 'r') or die('File '.$file_name.'can t be open');
-							$content = fread($handle, $file_size);
-							$content = chunk_split(base64_encode($content));
-							$f = fclose($handle);
-						 
+						  $message ='File is successfully uploaded.';
+
+						  //OK envoie du mail
+							
+
+							// To
+							$to = database::query("SELECT contact_mail FROM utilisateurs WHERE id=:user_id", array(':user_id'=>$annonce["user_id"]))[0]["contact_mail"];
+							 
+							// Subject
+							$subject = 'étu-stage - Nouvelle Candidature [' . $annonce["titre"] . ']';
+							 
+							// clé aléatoire de limite
+							$boundary = md5(uniqid(microtime(), TRUE));
+							 
+							// Headers
+							$headers = 'From: étu-stage <admin@vykio.fr>'."\r\n";
+							$headers .= 'Mime-Version: 1.0'."\r\n";
+							$headers .= 'Content-Type: multipart/mixed;boundary='.$boundary."\r\n";
+							$headers .= "\r\n";
+							 
+							// Message pour les clients ne supportant pas le type MIME
+							$msg = 'Texte affiché par des clients mail ne supportant pas le type MIME.'."\r\n\r\n";
+							 
+							// Message HTML
 							$msg .= '--'.$boundary."\r\n";
-							$msg .= 'Content-type:'.$file_type.';name='.'CV.'.$fileExtension."\r\n";
-							$msg .= 'Content-transfer-encoding:base64'."\r\n\r\n";
-							$msg .= $content."\r\n";
-						}
-						 
-						// Fin
-						$msg .= '--'.$boundary."\r\n";
-						 
-						// Function mail()
-						$mail_sent = mail($to, $subject, $msg, $headers);
-						if ($mail_sent) {
-							unlink($dest_path);
-						} else {
-							header('Location: ' . INDEX_PAGE);
+							$msg .= 'Content-type: text/html; charset=utf-8'."\r\n\r\n";
+							$msg .= '<div style="position:absolute width=100% height="300"">
+
+	                                <center><img src="https://vykio.fr/imgs/logo1.png" style="margin: 0; border: 0; padding: 0; display: block;" width="500"></center>
+	                                </div>
+	                                
+	                                <center><h2>Nouvelle candidature sur votre annonce de stage '. $annonce["titre"] .'.</h2></center>
+	                                
+	                                <p>&emsp;Bonjour, </p>
+	                                
+	                                
+	                                <p>Vous avez posté sur notre site une annonce de stage.</p>
+	                                <p>Bonne nouvelle un candidat semble intéressé, vous trouverez en pièce jointe son <b>CV</b>.<p>
+	                                
+	                                <h3>Vous pouvez le contacter par mail: ' . $email_of_user .'</h3>
+	                                <h4>Motivation: </h4>
+	                                <pre><code style="padding: .2rem .5rem;
+	                                  margin: 0 .2rem;
+	                                  font-size: 1.1em;
+	                                  display: block;
+	                                  white-space: pre;
+	                                  background: #F1F1F1;
+	                                  border: 1px solid #E1E1E1;
+	                                  border-radius: 4px;"><span style="white-space: pre-line;">'. $motivation .'</span></code></pre>
+	                                '."\r\n";
+							 
+							// Pièce jointe 1
+							$file_name = $dest_path;
+							if (file_exists($file_name))
+							{
+								$file_type = filetype($file_name);
+								$file_size = filesize($file_name);
+							 
+								$handle = fopen($file_name, 'r') or die('File '.$file_name.'can t be open');
+								$content = fread($handle, $file_size);
+								$content = chunk_split(base64_encode($content));
+								$f = fclose($handle);
+							 
+								$msg .= '--'.$boundary."\r\n";
+								$msg .= 'Content-type:'.$file_type.';name='.'CV.'.$fileExtension."\r\n";
+								$msg .= 'Content-transfer-encoding:base64'."\r\n\r\n";
+								$msg .= $content."\r\n";
+							}
+							 
+							// Fin
+							$msg .= '--'.$boundary."\r\n";
+							 
+							// Function mail()
+							$mail_sent = mail($to, $subject, $msg, $headers);
+							if ($mail_sent) {
+								unlink($dest_path);
+							} else {
+								header('Location: ' . INDEX_PAGE);
+								die(); 
+
+							}
+
+
+							
+							header('Location: ' . ANNONCE_PAGE);
 							die(); 
 
 						}
+						else
+						{
+						  $message = 'There was some error moving the file to upload directory. Please make sure the upload directory is writable by web server.';
+							echo $message;
+						}
 
-
-						
-						header('Location: ' . ANNONCE_PAGE);
-						die(); 
-
-					}
-					else
-					{
-					  $message = 'There was some error moving the file to upload directory. Please make sure the upload directory is writable by web server.';
-						echo $message;
 					}
 
 				}
@@ -262,12 +289,11 @@ if (isset($_GET['id']) && !empty($_GET['id'])) {
 				<div class="row">
 				<div class="six columns">
 					  <label>Email</label>
-					  <input class="u-full-width" type="email" placeholder=".....@mail.com" >
+					  <input class="u-full-width" type="email" name="email_of_user" placeholder=".....@mail.com" >
 					</div>
 
 					<div class="six columns">
 						<div class= "row">
-							<label>CV Upload</label>
 							<!--<label for="exampleEmailInput">CV Upload</label>
 							<button>chercher le fichier</button>
 							<input class="button-primary" type="button" value=" Telechargement">-->
@@ -280,7 +306,7 @@ if (isset($_GET['id']) && !empty($_GET['id'])) {
 				</div>
 
 						 <label>Message</label>
-						 <textarea class="u-full-width" placeholder="Votre motivation en quelques lignes" id="exampleMessage"></textarea>
+						 <textarea class="u-full-width" placeholder="Votre motivation en quelques lignes" id="exampleMessage" name="motivation"></textarea>
 						 <label class="example-send-yourself-copy">
 						  <input type="checkbox">
 						  <span class="label-body">Envoi d'une confirmation par mail</span>
