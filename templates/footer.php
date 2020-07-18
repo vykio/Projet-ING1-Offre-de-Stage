9cd4ab6db44a924fc1d30b90c932d1bda8dc018a
Alexandre <alexandrevasseur06@gmail.com>
2020-07-18 18:02:00 +0200

    Update footer.php

--- templates/footer.php
+++ templates/footer.php
@@ -1,3 +1,3 @@
 <div class="footer"> <?php // Il faut global.css  ?>
-	Projet informatique - 1ère année Ecole d'Ingénieurs du Littoral Côte d'Opale - Groupe TP12&emsp;<a href="https://github.com/vykio/Projet-ING1-Offre-de-Stage" target="_blank" style="text-decoration: none; color: black"><i class="fab fa-github"></i></a>
+	Projet informatique - 1ère année ING1 - Groupe TP12&emsp;<a href="https://github.com/vykio/Projet-ING1-Offre-de-Stage" target="_blank" style="text-decoration: none; color: black"><i class="fab fa-github"></i></a>
 </div>
